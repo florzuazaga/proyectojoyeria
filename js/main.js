b92c1cc7cd41b4923e8aabdf082febbf5f6e1826
florencia <florenciazuazaga47@gmail.com>
2023-06-19 19:34:35 -0300

    cambiosrequeridostres

--- js/main.js
+++ js/main.js
@@ -1,110 +1,22 @@
 const product = document.getElementById("conteinerproductos");
 const verCarrito = document.getElementById("vercarrito");
 const modalCarrito = document.getElementById("modal-carrito");
-const productos = [
-  {
-    id: "alianza",
-    titulo: "Alianza ",
-    imagen: "./img/alianza.jpg",
-    categoria: {
-      nombre: "Alianza",
-      id: "alianza",
-    },
-    cantidad: 1,
-    precio: 95258,
-  },
-  {
-    id: "reloj",
-    titulo: "Reloj",
-    imagen: "./img/reloj.png",
-    categoria: {
-      nombre: "Reloj",
-      id: "reloj",
-    },
-    cantidad: 1,
-    precio: 32000,
-  },
-  {
-    id: "abridores",
-    titulo: "Abridores de oro 18kts",
-    imagen: "./img/abridores.jpg",
-    categoria: {
-      nombre: "Abridores",
-      id: "abridores",
-    },
-    cantidad: 1,
-    precio: 28270,
-  },
-  {
-    id: "dije",
-    titulo: "Dije de oro 18kts",
-    imagen: "./img/dije.jpg",
-    categoria: {
-      nombre: "Dije",
-      id: "dije",
-    },
-    cantidad: 1,
-    precio: 46233,
-  },
-  {
-    id: "cadena",
-    titulo: "Cadena de plata 925",
-    imagen: "./img/cadena.jpg",
-    categoria: {
-      nombre: "Cadena",
-      id: "cadena",
-    },
-    cantidad: 1,
-    precio: 4670,
-  },
-  {
-    id: "anillo",
-    titulo: "Anillo de oro 18kts con topacio sintético central",
-    imagen: "./img/anillo.webp",
-    categoria: {
-      nombre: "Anillo",
-      id: "anillo",
-    },
-    cantidad: 1,
-    precio: 105075,
-  },
-  {
-    id: "aros",
-    titulo: "Aros de oro ammarillo y blanco 18kts combinados",
-    imagen: "./img/aros.jpg",
-    categoria: {
-      nombre: "Aros",
-      id: "aros",
-    },
-    cantidad: 1,
-    precio: 168120,
-  },
-  {
-    id: "pulsera",
-    titulo: "Pulsera de oro 18kts-tipo gucci",
-    imagen: "./img/pulsera.jpg",
-    categoria: {
-      nombre: "pulsera",
-      id: "pulsera",
-    },
-    cantidad: 1,
-    precio: 542187,
-  },
-];
-let carrito = [];
 const conteinerProductos = document.querySelector("#conteinerproductos");
-function cargarCarrito(productos) {
-  conteinerProductos.innerHTML = "";
-  productos.forEach((producto) => {
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+const getProducto = async () => {
+  const respuesta = await fetch("./productos.json");
+  const datos = await respuesta.json();
+  datos.forEach((producto) => {
     let content = document.createElement("div");
     content.className = "conteiner";
     content.innerHTML = `
-      <img class="producto-imagen"src="${producto.imagen}" alt="${producto.titulo}">
-    <div class="producto-detalles">
-      <h3 class="producto-titulo">${producto.titulo}</h3>
-      <p class="producto-precio">$${producto.precio}</p>
-      <p>cantidad: ${producto.cantidad}</p>
-  `;
+        <img class="producto-imagen"src="${producto.imagen}" alt="${producto.titulo}">
+      <div class="producto-detalles">
+        <h3 class="producto-titulo">${producto.titulo}</h3>
+        <p class="producto-precio">$${producto.precio}</p>
+        <p>cantidad: ${producto.cantidad}</p>
+    `;
     product.append(content);
 
     let agregar = document.createElement("button");
@@ -131,31 +43,34 @@ function cargarCarrito(productos) {
           precio: producto.precio,
           cantidad: producto.cantidad,
         });
+        savelocal();
       }
     });
   });
-}
-const tituloProducto = document.querySelector("#tituloproducto");
-const botonesCategorias = document.querySelectorAll(".boton-categoria");
-botonesCategorias.forEach((boton) => {
-  boton.addEventListener("click", (e) => {
-    botonesCategorias.forEach((boton) => boton.classList.remove("active"));
-    e.currentTarget.classList.add("active");
-    if (e.currentTarget.id != "todos") {
-      const productoCategoria = productos.find(
-        (producto) => producto.categoria.id === e.currentTarget.id
-      );
-      tituloProducto.innerText = productoCategoria.categoria.nombre;
-      const productosBoton = productos.filter(
-        (producto) => producto.categoria.id === e.currentTarget.id
-      );
-      cargarCarrito(productosBoton);
-    } else {
-      tituloProducto.innerText = "Todos los productos";
-      cargarCarrito(productos);
-    }
+  const tituloProducto = document.querySelector("#tituloproducto");
+  const botonesCategorias = document.querySelectorAll(".boton-categoria");
+  botonesCategorias.forEach((boton) => {
+    boton.addEventListener("click", (e) => {
+      botonesCategorias.forEach((boton) => boton.classList.remove("active"));
+      e.currentTarget.classList.add("active");
+      if (e.currentTarget.id != "todos") {
+        const productoCategoria = productos.find(
+          (producto) => producto.categoria.id === e.currentTarget.id
+        );
+        tituloProducto.innerText = productoCategoria.categoria.nombre;
+        const productosBoton = productos.filter(
+          (producto) => producto.categoria.id === e.currentTarget.id
+        );
+        cargarCarrito(productosBoton);
+      } else {
+        tituloProducto.innerText = "Todos los productos";
+        cargarCarrito(productos);
+      }
+    });
   });
-});
+};
+getProducto();
+
 const vermicarrito = () => {
   modalCarrito.innerHTML = ``;
   modalCarrito.style.display = "flex";
@@ -180,10 +95,27 @@ const vermicarrito = () => {
     carritoContent.innerHTML = `
       <h3 >${producto.titulo}</h3>
       <p>$${producto.precio}</p>
+      <span id="restar"> - </span>
       <p>${producto.cantidad}</p>
+      <span id="sumar"> + </span>
       <p>${producto.cantidad * producto.precio}</p>
     `;
     modalCarrito.append(carritoContent);
+    let restar = carritoContent.querySelector("#restar");
+    restar.addEventListener("click", () => {
+      if (producto.cantidad !== 1) {
+        producto.cantidad--;
+      }
+      vermicarrito();
+    });
+    let sumar = carritoContent.querySelector("#sumar");
+    sumar.addEventListener("click", () => {
+      if (producto.cantidad !== 1) {
+        producto.cantidad++;
+      }
+      vermicarrito();
+    });
+
     let eliminar = document.createElement("span");
     eliminar.innerText = "⚔";
     eliminar.className = "eliminar-producto";
@@ -203,5 +135,9 @@ const eliminarAgregado = () => {
   carrito = carrito.filter((compraId) => {
     return compraId !== foundId;
   });
+  savelocal();
   vermicarrito();
+};
+const savelocal = () => {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
 };

fec087cd5ebc1ac4fa06beb2a0544db625898136
florencia <florenciazuazaga47@gmail.com>
2023-06-26 12:12:43 -0300

    cambios5

--- js/main.js
+++ js/main.js
@@ -27,6 +27,26 @@ const getProducto = async () => {
     content.append(agregar);
 
     agregar.addEventListener("click", () => {
+      Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem",
+        },
+        offset: {
+          x: "1.5rem", // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+          y: "1.5rem", // vertical axis - can be a number or a string indicating unity. eg: '2em'
+        },
+        onClick: function () {}, // Callback after click
+      }).showToast();
+
       const repeat = carrito.some(
         (repeatProducto) => repeatProducto.id === producto.id
       );
